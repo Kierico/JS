0c70884b69c407153147f23ba2c15478416fdbae
kierico <kiericograndao@gmail.com>
2021-09-30 14:14:58 -0400

    Início aula 06

--- Seção 1 - Introdução ao JavaScript/Aula 5 - Intro JS Função e Objeto/calculadora.js
+++ Seção 1 - Introdução ao JavaScript/Aula 5 - Intro JS Função e Objeto/calculadora.js
@@ -1,5 +1,6 @@
 function calculadora(x1, x2, operator) {
 
+    // eval - função para que o JavaScript interprete uma string como um script.
     return eval(`${x1} ${operator} ${x2}`); // Evaluates - Avalia o código JavaScript e o executa.
 
 }
